e2807926bd9b78bbf12d580187b2d84dea06a100
Alexandre <Alexandre@Krabbe-PC>
2018-05-21 21:39:40 -0400

    First test implemented using testfx

--- src/test/java/gui/CalcTDDTests.java
+++ src/test/java/gui/CalcTDDTests.java
@@ -1,34 +1,39 @@
 package gui;
 
-import javafx.application.Application;
+import br.com.calctdd.calctddmaven.controller.FXMLDocumentController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
-
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.matcher.control.LabeledMatchers.hasText;
-
+import static org.junit.Assert.*;
+import org.loadui.testfx.GuiTest;
 import org.testfx.framework.junit.ApplicationTest;
-import org.testfx.robot.Motion;
 /**
  * @author Alexandre
  */
 public class CalcTDDTests extends ApplicationTest  {
     
+    private static FXMLDocumentController controller;
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent mainNode = FXMLLoader.load(getClass().getResource("/fxml/FXMLDocument.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent mainNode = fxmlLoader.load(getClass().getResource("/fxml/FXMLDocument.fxml"));
         mainNode.getStylesheets().add("styles/myStyles.css");
         primaryStage.setScene(new Scene(mainNode));
         primaryStage.show();
         primaryStage.toFront();
+        controller = (FXMLDocumentController) fxmlLoader.getController();
     }
     
     @Test
     public void testPressDigit() {
-        clickOn("#nineBtn");
+        TextField result = (TextField) GuiTest.find("#resultTextField");
+        clickOn("#nineBtn").clickOn("#plusBtn").clickOn("#twoBtn").clickOn("#equalsBtn");
+        assertThat(result.getText(), is("11"));
     }    
     
 }
